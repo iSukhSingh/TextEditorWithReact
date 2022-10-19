dfc97a5256a477a876a9d8280129d5b41cf20840
cali1204 <qwertyuiop.s1204@gmail.com>
2022-10-19 16:38:18 -0400

    Configure React app for deployment to GitHub Pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,9 +13,12 @@ export default function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                            <Link className="nav-link" aria-current="page" to="">Home</Link>
                         </li>
                         
+                        <li className="nav-item">
+                            <a target="_blank" className="nav-link" href="https://github.com/iSukhSingh">Github</a>
+                        </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/about">{props.aboutText}</Link>
                         </li>
